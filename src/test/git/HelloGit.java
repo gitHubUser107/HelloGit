93a8722089e5c38f142d6ab3aa1cf87a60fae7ca
Paul <paul24vicks@gmail.com>
2020-03-02 01:03:10 -0500

    Initial commit

---
+++ src/test/git/HelloGit.java
@@ -0,0 +1,14 @@
+package test.git;
+
+public class HelloGit {
+	
+	public static void main(String [] args) {
+		sayHelloGit();
+	}
+
+	private static void sayHelloGit() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");		
+	}
+
+}
