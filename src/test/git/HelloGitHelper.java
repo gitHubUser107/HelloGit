5fd8e82386555d7b840be9261bd7e3e28dc15a8b
Paul <paul24vicks@gmail.com>
2020-03-18 00:24:10 -0400

    now using a Logger

--- src/test/git/HelloGitHelper.java
+++ src/test/git/HelloGitHelper.java
@@ -1,8 +1,12 @@
 package test.git;
 
+import java.util.logging.Logger;
+
 public class HelloGitHelper {
 
+	private static final Logger logger = Logger.getLogger(HelloGitHelper.class.getName());
+	
 	public void sayHelloGit() {
-		System.out.println("Hello Git User");		
+		logger.info("Hello Git User.... Do you like java 11 ?");		
 	}
 }
